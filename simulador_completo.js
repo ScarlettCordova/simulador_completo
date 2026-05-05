2240690476f0debb5b506be3f18a86372b5c432f
ScarlettCordova <scarargecorb@gmail.com>
2026-05-04 21:13:32 -0500

    Se agregó las ultimas funciones para acabar el taller

--- simulador_completo.js
+++ simulador_completo.js
@@ -54,4 +54,77 @@ function guardarCliente(){
   console.log(cmpApellido)
   console.log(cmpIngresos)
   console.log(cmpEgresos)
+  let cliente={
+    cedula:cmpCedula,
+    nombre:cmpNombre,
+    apellido: cmpApellido,
+    ingresos: cmpIngresos,
+    egresos: cmpEgresos
+  }
+    console.log(cliente)
+
+    if(clienteSeleccionado!=null){
+      if(clientes[clienteSeleccionado].cedula!=cliente.cedula){
+        alert("NO SE PERMITE MODIFICAR LA CEDULA")
+      }
+      clientes[clienteSeleccionado].nombre=cliente.nombre
+      clientes[clienteSeleccionado].apellido=cliente.apellido
+      clientes[clienteSeleccionado].ingresos=cliente.ingresos
+      clientes[clienteSeleccionado].egresos=cliente.egresos
+    }else{
+    clientes.push(cliente)// se guarda el objeto
+    console.log(clientes)
+    console.log(clientes.toString())
+    }
+    pintarCliente()
+    limpiar()
+}
+
+function pintarCliente (){
+  let tabla=document.getElementById("tablaClientes");
+  let contenidoTabla="<tr>"
+  for (let i=0;i<clientes.length;i++){
+    let objCliente=clientes[i];
+    contenidoTabla+="<td>"+objCliente.cedula+"</td>"
+    contenidoTabla+="<td>"+objCliente.nombre+"</td>"
+    contenidoTabla+="<td>"+objCliente.apellido+"</td>"
+    contenidoTabla+="<td>"+objCliente.ingresos+"</td>"
+    contenidoTabla+="<td>"+objCliente.egresos+"</td>"
+    contenidoTabla+="<td>"+
+            "<button onclick=seleccionarCliente("+objCliente.cedula+")>Actualizar</button>"+
+            "<button>Eliminar</button>"+
+          "</td></tr>"
+  }
+
+  tabla.innerHTML=contenidoTabla;
+}
+
+function buscarCliente(cedula){
+  for (let i=0;i<clientes.length;i++){
+    let objCliente=clientes[i];
+    if(objCliente.cedula==cedula){
+      return i
+    }
+  }
+  return null
+}
+
+function seleccionarCliente(cedula){
+  clienteSeleccionado=buscarCliente(cedula)
+  if(clienteSeleccionado!=null){
+    mostrarTextoEnCaja("cedula",clientes[clienteSeleccionado].cedula)
+    mostrarTextoEnCaja("nombre",clientes[clienteSeleccionado].nombre)
+    mostrarTextoEnCaja("apellido",clientes[clienteSeleccionado].apellido)
+    mostrarTextoEnCaja("ingresos",clientes[clienteSeleccionado].ingresos)
+    mostrarTextoEnCaja("egresos",clientes[clienteSeleccionado].egresos)
+  }
+}
+
+function limpiar(){
+   mostrarTextoEnCaja("cedula","")
+    mostrarTextoEnCaja("nombre","")
+    mostrarTextoEnCaja("apellido","")
+    mostrarTextoEnCaja("ingresos","")
+    mostrarTextoEnCaja("egresos","")
+    clienteSeleccionado=null
 }
